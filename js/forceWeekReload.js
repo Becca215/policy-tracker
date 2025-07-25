8135e8711fd6793886cc30e6d92562dea70cf2c0
Becca <estrellainsdocs@gmail.com>
2025-07-24 17:27:09 -0700

    Delete js directory

--- js/forceWeekReload.js
+++
@@ -1,9 +0,0 @@
-// This script forces a full page reload when the week is changed
-window.addEventListener('DOMContentLoaded', () => {
-  const weekInput = document.getElementById('weekStartInput');
-  if (weekInput) {
-    weekInput.addEventListener('change', () => {
-      location.reload();
-    });
-  }
-});
